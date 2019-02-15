b74101eafed0c974d52ad2131aec2488d73f62a3
wojciechlasak <wojciech.laska@student.uj.edu.pl>
2019-02-15 17:24:23 +0100

    add buttons for each components

--- client/src/Admin.js
+++ client/src/Admin.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import Calendar from "./Calendar.js";
+import CalendarAdmin from "./CalendarAdmin.js";
+import ClientForm from "./ClientForm.js";
 import Data from "./Data.js";
 
 /* Once the 'Authservice' and 'withAuth' componenets are created, import them into App.js */
@@ -10,6 +12,13 @@ import AuthHelperMethods from "./components/AuthHelperMethods";
 import withAuth from "./components/withAuth";
 
 class Admin extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      component: null
+    };
+  }
+
   /* Create a new instance of the 'AuthHelperMethods' compoenent*/
   Auth = new AuthHelperMethods();
 
@@ -18,13 +27,52 @@ class Admin extends React.Component {
     this.props.history.replace("/");
   };
 
+  componentChange = e => {
+    this.setState({
+      component: e.target.value
+    });
+  };
+  componentRender() {
+    switch (this.state.component) {
+      case "clients":
+        return <Data />;
+      case "reservations":
+        return <CalendarAdmin />;
+      case "addReservations":
+        return (
+          <div>
+            <Calendar />
+            <ClientForm />
+          </div>
+        );
+      default:
+        return null;
+    }
+  }
+
   //Render the protected component
   render() {
     return (
       <div>
-        <Calendar />
-        <button onClick={this._handleLogout}>LOGOUT</button>
-        <Data />
+        <nav>
+          <button value="clients" onClick={this.componentChange}>
+            Przegląd klientów
+          </button>
+          <button value="addReservations" onClick={this.componentChange}>
+            Dodaj rezerwacje
+          </button>
+          <button value="opinions" onClick={this.componentChange}>
+            Przegląd Opinii
+          </button>
+          <button value="reservations" onClick={this.componentChange}>
+            Przegląd rezerwacji
+          </button>
+          <button value="rooms" onClick={this.componentChange}>
+            Przegląd pokoi
+          </button>
+          <button onClick={this._handleLogout}>LOGOUT</button>
+        </nav>
+        {this.componentRender()}
       </div>
     );
   }
