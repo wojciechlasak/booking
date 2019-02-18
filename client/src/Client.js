9e255107bd7b3bc8b73e3969173ad3d84797d990
wojciechlasak <wojciech.laska@student.uj.edu.pl>
2019-02-18 22:46:52 +0100

    add callback for all part

--- client/src/Client.js
+++ client/src/Client.js
@@ -1,42 +1,83 @@
 import React from "react";
 
-import Calendar from './Calendar.js'
-import ClientForm from './ClientForm.js'
-import Select from './Select.js'
-import RoomMap from './RoomMap.js'
+import Calendar from "./Calendar.js";
+import ClientForm from "./ClientForm.js";
+import ClientSelect from "./ClientSelect.js";
+import RoomMap from "./RoomMap.js";
 
 import "./css/Client.css";
 
 class Client extends React.Component {
-    constructor(){
-        super();
-        this.state= {
-          from : "",
-          to: "",
-          hide:true
-        }
-      }
-      getData(param1,param2) {
-        this.setState({
-          from : param1,
-          to: param2,
-          hide: false
-        })
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      from: null,
+      to: null,
+      hideMap: true,
+      hideForm: true,
+      peopleAmount: null,
+      roomsAvailable: null,
+      roomsAmount: null,
+      roomsChose: null
+    };
 
-    render() {
-        return (
-            <div id="client">
-                <div id="top">
-                    <Calendar callback={this.getData.bind(this)}/>
-                    <Select dateFrom={this.state.from} dateTo={this.state.to}/>
-                </div>
-                <div id="map">
-                    <RoomMap/>
-                </div>
-                {!this.state.hide?<ClientForm dateFrom={this.state.from} dateTo={this.state.to}/>:""}
-            </div>
-        );
-    }
+    this.getDate = this.getDate.bind(this);
+    this.getSelect = this.getSelect.bind(this);
+    this.getMap = this.getMap.bind(this);
+  }
+  getDate(param1, param2) {
+    this.setState({
+      from: param1,
+      to: param2
+    });
+  }
+
+  getSelect(param1, param2, param3) {
+    this.setState({
+      peopleAmount: param1,
+      roomsAvailable: param2,
+      roomsAmount: param3,
+      hideMap: false
+    });
+  }
+  getMap(param) {
+    this.setState({
+      roomsChose: param,
+      hideForm: false
+    });
+    
+  }
+
+  render() {
+    return (
+      <div id="client">
+        <div id="top">
+          <Calendar callback={this.getDate} />
+          <ClientSelect
+            callbackSelect={this.getSelect}
+            dateFrom={this.state.from}
+            dateTo={this.state.to}
+          />
+        </div>
+        {!this.state.hideMap ? (
+          <div id="map">
+            <RoomMap
+              roomsAmount={this.state.roomsAmount}
+              roomsAvailable={this.state.roomsAvailable}
+              callbackMap={this.getMap}
+            />
+          </div>
+        ) : null}
+        {!this.state.hideForm ? (
+          <ClientForm
+            roomsChose={this.state.roomsChose}
+            peopleAmount={this.state.peopleAmount}
+            dateFrom={this.state.from}
+            dateTo={this.state.to}
+          />
+        ) : null}
+      </div>
+    );
+  }
 }
 export default Client;
