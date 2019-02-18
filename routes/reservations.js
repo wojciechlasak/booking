978c17c4410fb00bfa46ba3e0c0d308dbe1b79e3
wojciechlasak <wojciech.laska@student.uj.edu.pl>
2019-02-18 22:48:48 +0100

    minor change in post

--- routes/reservations.js
+++ routes/reservations.js
@@ -32,8 +32,8 @@ router.get("/reservation/:reservationId", function(req, res) {
 router.post("/", function(req, res) {
   con.query(
     `INSERT INTO reservation VALUES ('${req.body.id}', 0, '${
-      req.body.dateTo
-    }', '${req.body.dateFrom}',"", 3, ${req.body.roomNr},${req.body.clientId})`,
+      req.body.dateFrom
+    }', '${req.body.dateTo}',"", ${req.body.peopleAmount}, ${req.body.roomNr},${req.body.clientId})`,
     function(error, results, fields) {
       if (error) throw error;
       res.send(JSON.stringify(results));
