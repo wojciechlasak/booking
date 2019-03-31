71c028d7704a906352cfb31bd4db953ebc881518
wojciechlasak <wojciech.laska@student.uj.edu.pl>
2019-03-31 21:15:28 +0200

    add input and fetch code

--- client/src/CheckCode.js
+++ client/src/CheckCode.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Button, Form, FormGroup, Input } from "reactstrap";
 
 import "./css/CheckCode.css";
+
 
 class CheckCode extends React.Component {
   constructor(props) {
@@ -9,9 +11,56 @@ class CheckCode extends React.Component {
   }
   getInitialState() {
     return {
-      isOpen: false
+      code:null,
+      isOpen: false,
+      isCorrectCode:false,
+      reservation:null,
     };
   }
+
+  generateInfo(){
+    var arr = [];
+    arr.push(<div key="0">ID: {this.state.reservation[0].reservation_id}<br/>
+      Od {this.state.reservation[0].dateFrom.slice(0,10)} - {this.state.reservation[0].dateTo.slice(0,10)}<br/>
+      Do zapłacenia {this.state.reservation[0].price}</div>)
+    for(let singleReservation of this.state.reservation){
+        arr.push(<div>{singleReservation.room_nr}</div>)
+      }
+    return arr;
+  }
+
+  handleSubmit = () =>{
+    fetch(`/reservations/${this.state.code}`,{method:"GET"})
+    .then(function(response) {
+      if (response.status >= 400) {
+        var error = new Error(response.statusText)
+        error.response = response
+        throw error
+      }
+      return response.json();
+    })
+    .then(data => {
+      if(data.length){
+        this.setState({
+          isCorrectCode:true,
+          reservation:data,
+        })
+      }
+      else{
+        //alert
+      }
+    })
+    .catch(err => {
+      console.log("caught it!", err);
+    });
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+
 
   openLightbox = () =>{
     this.setState({
@@ -20,9 +69,7 @@ class CheckCode extends React.Component {
   }
 
   closeLightbox = () => {
-    this.setState({
-      isOpen: false,
-    })
+    this.setState(this.getInitialState())
   }
 
   render() {
@@ -38,6 +85,19 @@ class CheckCode extends React.Component {
       <div id="reservation-lightbox-container" className={this.state.isOpen?"opened":" "}>
         <div id="reservation-lightbox-bg" onClick={this.closeLightbox}/>
         <div id="reservation-lightbox-in">
+        {!this.state.isCorrectCode?<Form method="post" className="Login" onSubmit={this.handleSubmit}>
+            <FormGroup>
+              <Input
+                autoFocus
+                type="text"
+                placeholder="Wpisz swój kod"
+                id="code"
+                value={this.state.code || ""}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            <Button onClick={this.handleSubmit}>Sprawdź</Button>
+          </Form>:<div>{this.generateInfo()}</div>}
           <svg id="reservation-lightbox-close" onClick={this.closeLightbox} viewBox="0 0 100 100">
             <path d="M10 10L90 90" />
             <path d="M90 10L10 90" />
